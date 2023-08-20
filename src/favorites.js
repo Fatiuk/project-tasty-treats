a94d02ac800f9ca176ecfd411864965642bdeedc
Alona <mykyshka2013@gmail.com>
2023-08-20 20:41:10 +0300

    update favorites

---
+++ src/favorites.js
@@ -0,0 +1,10 @@
+//* This file contains functions for interacting
+//* with the Tasty Treats backend API.
+import './js/request-handler.js';
+
+//* Toggle the theme color between light and dark mode.
+import './js/toggle-theme.js';
+
+import './js/mobile.js';
+
+import './js/scroll-to-top.js';
