e001e287facc905f99ad9ca279be14ae2a92426b
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-08-21 17:24:03 +0300

    header_update

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -1,33 +1,68 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const themeToggle = document.querySelector('.checkbox');
+  const themeToggle1 = document.querySelector('.checkbox');
+  const themeToggle2 = document.querySelector('.checkbox-burger');
   const body = document.body;
 
-  const selectedTheme = localStorage.getItem('theme');
-  if (selectedTheme === 'dark') {
-    applyDarkTheme();
-    themeToggle.checked = true;
-  } else {
-    applyLightTheme();
-    themeToggle.checked = false;
-  }
+  themeToggle1.addEventListener('click', toggleTheme);
+  themeToggle2.addEventListener('click', toggleTheme);
 
-  themeToggle.addEventListener('click', function () {
+  function toggleTheme() {
     if (body.classList.contains('dark-theme')) {
-      applyLightTheme();
-      localStorage.setItem('theme', 'light');
+      applyTheme('light');
     } else {
-      applyDarkTheme();
-      localStorage.setItem('theme', 'dark');
+      applyTheme('dark');
     }
-  });
-
-  function applyLightTheme() {
-    body.classList.remove('dark-theme');
-    body.classList.add('light-theme');
   }
 
-  function applyDarkTheme() {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
+  function applyTheme(theme) {
+    body.classList.remove('dark-theme', 'light-theme');
+    body.classList.add(theme + '-theme');
+    localStorage.setItem('theme', theme);
   }
+
+  // Отримання поточної теми з localStorage та встановлення стану перемикачів
+  const selectedTheme = localStorage.getItem('theme');
+  applyTheme(selectedTheme);
+  themeToggle1.checked = selectedTheme === 'dark';
+  themeToggle2.checked = selectedTheme === 'dark';
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const themeToggle1 = document.querySelector('.checkbox');
+//   const themeToggle2 = document.querySelector('.checkbox-burger');
+//   const body = document.body;
+
+//   const selectedTheme = localStorage.getItem('theme');
+//   if (selectedTheme === 'dark') {
+//     applyDarkTheme();
+//     themeToggle1.checked = true;
+//     themeToggle2.checked = true;
+//   } else {
+//     applyLightTheme();
+//     themeToggle1.checked = false;
+//     themeToggle2.checked = false;
+//   }
+
+//   themeToggle1.addEventListener('click', toggleTheme);
+//   themeToggle2.addEventListener('click', toggleTheme);
+
+//   function toggleTheme() {
+//     if (body.classList.contains('dark-theme')) {
+//       applyLightTheme();
+//       localStorage.setItem('theme', 'light');
+//     } else {
+//       applyDarkTheme();
+//       localStorage.setItem('theme', 'dark');
+//     }
+//   }
+
+//   function applyLightTheme() {
+//     body.classList.remove('dark-theme');
+//     body.classList.add('light-theme');
+//   }
+
+//   function applyDarkTheme() {
+//     body.classList.remove('light-theme');
+//     body.classList.add('dark-theme');
+//   }
+// });
